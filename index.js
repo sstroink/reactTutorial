a093e993ef9e18639d54ea6d5f894e44290b7b7e
Sophie Stroink <sstroink@uvic.ca>
2018-01-18 15:03:28 -0800

    finished, screen shot upload

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+/*Game now:
+-lets you play tic-tac-toe,
+-indicates when one player has won the game,
+-stores the history of moves during the game,
+-allows players to jump back in time to see older versions of the game board.
+*/
 function Square(props){
     return(
         <button className="square" onClick={props.onClick}>
@@ -11,42 +16,25 @@ function Square(props){
 }
 
 class Board extends React.Component {
-    //sets inital state to have an array with 9 nulls
-    constructor(props) {
+    //sets inital state to have an array with 9 nulls, dont need anymore
+    /*constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
             xIsNext: true,
         };
-    }
+    }*/
     
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext, 
-        });
-    }
   renderSquare(i) {
-    return (<Square value={this.state.squares[i]}
-            onClick={()=> this.handleClick(i)}
+    return (<Square value={this.props.squares[i]}
+            onClick={()=> this.props.onClick(i)}
             />
         );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -68,15 +56,72 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber+1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+        return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+            squares: squares,
+        }]),
+            //update the state in handleClick
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+    });
+  }
+    
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step, 
+            xIsNext: (step % 2) === 0,
+        });
+    }
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step,move) =>{
+        const desc = move ? 'Go to move #' + move : 'Go to game start';
+        return(
+            //for each step in history we create a list tiem with a button inside it that has a click handler. 
+            <li key={move}>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+      
+    let status;
+    if(winner){
+        status = 'Winner: ' + winner;
+    } else{
+        status= 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+        />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
